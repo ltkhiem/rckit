03527b87cded6fbe5378750fb151e260773c46f7
Tu-Khiem Le <khiemletu@gmail.com>
2022-02-22 15:37:47 +0000

    :hammer: [Dev] Saccade detection
    Detect saccade from a given list of fixations

--- ocular_movement_detection.py
+++ ocular_movement_detection.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from settings import _TH_FIXA_DUR
 
-def _detect_by_gazepoint_filter(df, th_fixa_dur=None, th_sacc_dur=None):
+def _detect_by_gazepoint_filter(df, th_fxdur=None, th_scdur=None):
     """ 
     Detect fixations, saccades and blinks using the annotation provided 
     by Gazepoint's internal filter.
@@ -14,7 +14,7 @@ def _detect_by_gazepoint_filter(df, th_fixa_dur=None, th_sacc_dur=None):
     -------
 
     """
-    def _detect_fixations(df, th_fixa_dur):
+    def _detect_fixations():
         fixa_groups = df.groupby(by=['FPOGID'])
         # Should we ignore the first fixation (as it could be the remaining 
         # of the last fixation in the previous session)?
@@ -22,22 +22,40 @@ def _detect_by_gazepoint_filter(df, th_fixa_dur=None, th_sacc_dur=None):
         session_time = df['TIME'].values[0]
         for fpogid, fpogs in fixa_groups:
             fpogs_valid = fpogs[fpogs['FPOGV'] == 1]
-            fixa_x = fpogs_valid['FPOGX'].mean()
-            fixa_y = fpogs_valid['FPOGY'].mean()
-            fixa_dur = fpogs_valid['FPOGD'].values[-1]
-            fixa_time = fpogs_valid['TIME'].values[0] - session_time
+            fxh = fpogs_valid['FPOGX'].mean()
+            fxv = fpogs_valid['FPOGY'].mean()
+            fxdur = fpogs_valid['FPOGD'].values[-1]
+            fxtime = fpogs_valid['TIME'].values[0] - session_time
 
-            if th_fixa_dur is not None and fixa_dur * 1000 <= th_fixa_dur: 
+            if th_fxdur is not None and fxdur * 1000 <= th_fxdur: 
                 # Fixation duration requirement not meet.
                 continue
 
-            if 0 <= fixa_x <= 1 and 0 <= fixa_y <= 1:
+            if 0 <= fxh <= 1 and 0 <= fxv <= 1:
                 # Only take fixation that is inside the screen.
-                fixations.append([fixa_x, fixa_y, fixa_dur, fixa_time])
+                fixations.append([fxh, fxv, fxdur, fxtime])
         return np.array(fixations)
 
+    def _detect_saccades():
+        print(fixations[0:4])
+        fxend = (fixations[:, 2] + fixations[:, 3]).reshape(-1,1)
+        print(fxend[:4])
+        time_diff = (fixations[1:][:, 3] - fxend[:-1, 0]).reshape(-1,1)
+        print(time_diff[:4])
+        saccades = np.concatenate([
+                fixations[:-1][:, :2],  # start_x, start_y
+                fixations[1:][:, :2],   # end_x, end_y
+                time_diff,              # saccades duration
+                fxend[:-1]               # saccades start will be end of last fixation
+            ], axis=1)
+        return saccades
 
-    print(len(fixations))
+    fixations = _detect_fixations()
+    saccades = _detect_saccades()
+    print(fixations[0:4])
+    print(saccades[0:4])
+    print(fixations.shape, saccades.shape)
+    return fixations, saccades
 
 
 def detect(df, method='gazepoint'):

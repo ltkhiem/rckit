3080057bcc41c6085da0903d1bf81b28c4076a49
Tu-Khiem Le <khiemletu@gmail.com>
2022-04-08 18:25:16 +0100

    :wrench: [Config] Update settings for ocular detector

--- settings.py
+++ settings.py
@@ -15,13 +15,13 @@ SUBJECT_PATHS = [DATA_PATH / sbj for sbj in SUBJECTS]
 ET_PATHS = [path / 'fixed_et' for path in SUBJECT_PATHS]
 EOG_PATHS = [path / 'fixed_eog' for path in SUBJECT_PATHS]
 INFO_PATHS = [path / 'fixed_info' for path in SUBJECT_PATHS]
-META_INFO_PATHS = [[path / f'metadata_log_{bid}.tsv') for bid in range(NUM_BLOCKS)] for path in INFO_PATHS]
+META_INFO_PATHS = [[path / f'metadata_log_{bid}.tsv' for bid in range(NUM_BLOCKS)] for path in INFO_PATHS]
 SCREENSHOT_PATHS = [path / 'screenshot' for path in SUBJECT_PATHS]
 
 # Settings for Eye Tracking
 ET_CALIB_PATHS = [path / 'tracker_calib_log.tsv' for path in ET_PATHS]
-ET_EVENT_PATHS = [path / 'tracker_event_log.tsv') for path in ET_PATHS]
-ET_DATA_PATHS = [[path / f'tracker_data_log_{bid}.tsv') for bid in range(NUM_BLOCKS)] for path in ET_PATHS]
+ET_EVENT_PATHS = [path / 'tracker_event_log.tsv' for path in ET_PATHS]
+ET_DATA_PATHS = [[path / f'tracker_data_log_{bid}.tsv' for bid in range(NUM_BLOCKS)] for path in ET_PATHS]
 
 # Reading behaviours
 CONDITION_TEXT = ['reading', 'scanning', 'skimming', 'proof_reading']
